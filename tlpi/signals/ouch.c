94b77abca1ea4bd885eeb7155a36b7534e75d10d
kdurant <wangjun850725@163.com>
2020-05-28 08:33:36 +0800

    sync

--- tlpi/signals/ouch.c
+++ tlpi/signals/ouch.c
@@ -4,6 +4,7 @@
 #include "error_functions.h"
 #include "tlpi_hdr.h"
 
+// demo 上每次<Ctrl-C> 都会进去这个函数
 static void sigHandler(int sig)
 {
     printf("Ouch!\n");
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
     for(j = 0;; j++)
     {
         printf("%d\n", j);
-        sleep(3);
+        sleep(1);
     }
     return 0;
 }
