6092407be0defe06681c94623ecd8684abd6942c
kdurant <wangjun850725@163.com>
2020-11-16 22:04:15 +0800

    chore: fail to send arp packet

--- network/arp/arp.c
+++ network/arp/arp.c
@@ -18,9 +18,9 @@ struct arp_packet
     unsigned char  ar_hln;
     unsigned char  ar_pln;
     unsigned short ar_op;
-    unsigned char  ar_sha[ETH_ALEN];
+    unsigned char  ar_src_mac[ETH_ALEN];
     unsigned char *ar_sip;
-    unsigned char  ar_tha[ETH_ALEN];
+    unsigned char  ar_target_mac[ETH_ALEN];
     unsigned char *ar_tip;
 };
 
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     char           ef[ETH_FRAME_LEN];
     struct ethhdr *p_ethhdr;
     char           eth_dest[ETH_ALEN]   = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    char           eth_source[ETH_ALEN] = {0x00, 0x0C, 0x29, 0x73, 0x9D, 0x15};
+    char           eth_source[ETH_ALEN] = {0x08, 0x60, 0x6e, 0x87, 0x36, 0xff};
 
     int sockfd = socket(AF_INET, SOCK_PACKET, htons(0x0003));
 
@@ -44,10 +44,10 @@ int main(int argc, char *argv[])
     p_arp->ar_pro = htons(0x8000);
     p_arp->ar_hln = 6;
     p_arp->ar_pln = 4;
-    memcpy(p_arp->ar_sha, eth_source, ETH_ALEN);
-    p_arp->ar_sip = (unsigned char *)inet_addr("192.168.1.152");
-    memcpy(p_arp->ar_tha, eth_dest, ETH_ALEN);
-    p_arp->ar_tip = (unsigned char *)inet_addr("192.168.1.1");
+    memcpy(p_arp->ar_src_mac, eth_source, ETH_ALEN);
+    p_arp->ar_sip = (unsigned char *)inet_addr("192.168.1.109");
+    memcpy(p_arp->ar_target_mac, eth_dest, ETH_ALEN);
+    p_arp->ar_tip = (unsigned char *)inet_addr("192.168.1.101");
 
     for(int i = 0; i < 8; i++)
     {
