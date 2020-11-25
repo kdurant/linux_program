c1eafa7b86744a2d7717f57f860491a5890d9d53
kdurant <wangjun850725@163.com>
2020-11-25 22:41:54 +0800

    feat: 使用argparse设置参数

--- network/basic_tcp/tcp_client.py
+++ network/basic_tcp/tcp_client.py
@@ -1,10 +1,16 @@
-#-*- coding: UTF-8 -*- 
+#-*- coding: UTF-8 -*-
+import argparse
 import socket
 import time
- 
+
+parser = argparse.ArgumentParser(description="simple tcp client program")
+parser.add_argument("--ip", default="127.0.0.1", help="tcp client ip address")
+parser.add_argument("--port", default=8888, type=int, help="tcp client port")
+args = parser.parse_args()
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1",8888))
- 
+client.connect((args.ip, args.port))
+
 while True:
     client.send(b'hello kdurant\n')
     time.sleep(1)
--- network/basic_tcp/tcp_server.py
+++ network/basic_tcp/tcp_server.py
@@ -1,16 +1,22 @@
+#-*- coding: UTF-8 -*- 
+import argparse
 import socket
 
-tcpServerSocket=socket.socket()
-host = socket.gethostname()
-print(host)
-port=8888
-tcpServerSocket.bind((host,port))
+parser = argparse.ArgumentParser(description="simple tcp server program")
+parser.add_argument("--ip", default="127.0.0.1", help="tcp server ip address")
+parser.add_argument("--port", default=8888, type=int, help="tcp server port")
+args = parser.parse_args()
+
+tcpServerSocket = socket.socket()
+tcpServerSocket.bind((args.ip, args.port))
 tcpServerSocket.listen(5)
 while True:
+    connection, addr = tcpServerSocket.accept()
+    print ('new client addr is : ', addr)
     
-    
-    c, addr = tcpServerSocket.accept()       
-    print ('client addr is : ', addr)
-    str = 'hello client'
-    #c.send(str)
-    c.close() 
+    while True:
+        data = connection.recv(1024)
+        print(data)
+    #connection.send(str)
+
+connection.close() 
