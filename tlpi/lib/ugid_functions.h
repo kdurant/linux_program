466656cfb6d6bc95abdfc602d59ab3cd8ab7967c
No <wangjun850725@163.com>
2024-10-05 19:30:59 +0800

    chore: 删除文件

--- tlpi/lib/ugid_functions.h
+++
@@ -1,9 +0,0 @@
-#include <sys/types.h>
-
-char* userNameFromId(uid_t uid);
-
-uid_t userIdFromName(const char* name);
-
-char* groupNameFromId(gid_t gid);
-
-gid_t groupIdFromName(const char* name);
